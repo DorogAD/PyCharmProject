338a587ca2324e5253c7fb80370b8e8bd91b6a14
DorogAD <sa_do@tut.by>
2021-02-11 11:50:11 +0300

    finish part 6

--- appname/views.py
+++ appname/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'appname/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'appname/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'appname/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'appname/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'appname/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'appname/results.html'
 
 
 def vote(request, question_id):
